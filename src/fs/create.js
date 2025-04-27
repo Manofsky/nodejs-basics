79ef9446b26f745c7a4c0958c1347d2c492e1fc8
manofsky <clegrof@gmail.com>
2025-04-27 23:40:39 +0300

    feat: impl create func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { join } from 'node:path';
+import { cwd } from 'node:process';
+import { writeFile } from 'node:fs/promises';
+
+const data = 'I am fresh and young';
+const fileName = 'fresh.txt';
+const folderPath = 'src/fs/files';
+
 const create = async () => {
     // Write your code here 
+    try {
+        await writeFile(join(cwd(), folderPath, fileName), data, { flag: 'wx' });
+    } catch (error) {
+        if (error.code === 'EEXIST') {
+            throw new Error('FS operation failed');
+        }
+        throw error;
+    }
 };
 
 await create();
