49f16b155ad995c00871d09e702fe8724473ff09
manofsky <clegrof@gmail.com>
2025-04-28 09:31:20 +0300

    feat: impl copy func

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { mkdir, readdir, copyFile } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const dataFolder = join(__dirname, 'files');
+const dataCopyFolder = join(__dirname, 'files_copy');
+
 const copy = async () => {
     // Write your code here 
+    try {
+        const files = await readdir(dataFolder);
+        await mkdir(dataCopyFolder, { recursive: false });
+
+        await Promise.all(files.map(file => copyFile(join(dataFolder, file), join(dataCopyFolder, file))));
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else if (error.code === 'EEXIST') {
+            throw new Error('FS operation failed');
+        }
+
+        throw error;
+    }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,15 +1,18 @@
-import { join } from 'node:path';
-import { cwd } from 'node:process';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { writeFile } from 'node:fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const data = 'I am fresh and young';
 const fileName = 'fresh.txt';
-const folderPath = 'src/fs/files';
+const folderPath = 'files';
 
 const create = async () => {
     // Write your code here 
     try {
-        await writeFile(join(cwd(), folderPath, fileName), data, { flag: 'wx' });
+        await writeFile(join(__dirname, folderPath, fileName), data, { flag: 'wx' });
     } catch (error) {
         if (error.code === 'EEXIST') {
             throw new Error('FS operation failed');
