c59556b81b901fc5c1b04189f1366bc0c96f59b2
manofsky <clegrof@gmail.com>
2025-04-28 21:27:50 +0300

    feat: add cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
     // Write your code here 
+    const args = process.argv.slice(2);
+    const parsedArgs = args.reduce((acc, cv, index) => {
+        if (cv.startsWith('--')) {
+            acc.push(`${cv.slice(2)} is ${args[index + 1]}`)
+        }
+        return acc;
+    }, []);
+
+    console.log(parsedArgs.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+const envPrefix = 'RSS_';
+
 const parseEnv = () => {
     // Write your code here 
+    const rssVariables = [];
+    for (const key in process.env) {
+        if (key.startsWith(envPrefix)) {
+            rssVariables.push(`${key}=${process.env[key]}`);
+        }
+    }
+
+    console.log(rssVariables.join('; '));
 };
 
 parseEnv();
