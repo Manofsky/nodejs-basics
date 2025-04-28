808f2c3681ed4b9746d9d4d551a316d466bc38e1
manofsky <clegrof@gmail.com>
2025-04-28 22:52:29 +0300

    feat: add streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createReadStream } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileToRead = join(__dirname, 'files/fileToRead.txt');
+
 const read = async () => {
     // Write your code here 
+    const input = createReadStream(fileToRead);
+    input.pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createWriteStream } from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileToWrite = join(__dirname, 'files/fileToWrite.txt');
+
 const write = async () => {
     // Write your code here 
+    const output = createWriteStream(fileToWrite);
+    process.stdin.pipe(output);
 };
 
 await write();
